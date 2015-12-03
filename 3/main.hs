be48d4921f870664727eae44734c14ecb63718a5
carmethene <carmethene@gmail.com>
2015-12-02 21:43:47 -0800

    Solution to 3.2

--- 3/main.hs
+++ 3/main.hs
@@ -13,8 +13,18 @@ move (x,y) ins
 houses :: House -> [Instruction] -> [House]
 houses = scanl move 
 
+split :: [a] -> ([a], [a])
+split [] = ([], [])
+split [x] = ([x], [])
+split (x:y:xys) = (x:xs, y:ys) where (xs, ys) = split xys
+
 main = do
     instructions <- getLine
-    let presents = houses (0,0) instructions
-    print $ "Num houses: " ++ show (length $ nub presents)
+    let startHouse = (0,0)
+    -- part 1
+    let presents = houses startHouse instructions
+    print $ "Num houses (1): " ++ show (length $ nub presents)
+    -- part 2
+    let splitPresents = houses startHouse ins1 ++ houses startHouse ins2 where (ins1, ins2) = split instructions
+    print $ "Num houses (2): " ++ show (length $ nub splitPresents)
 
