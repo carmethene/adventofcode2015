aaffbd7a6d40fd42455130659a9522f6ddf239f0
carmethene <carmethene@gmail.com>
2015-12-04 10:40:10 -0800

    Solution to 4.2

--- 4/main.hs
+++ 4/main.hs
@@ -11,7 +11,12 @@ startsWithFiveZeros :: String -> Bool
 startsWithFiveZeros ('0':'0':'0':'0':'0':_) = True
 startsWithFiveZeros _ = False
 
+startsWithSixZeros :: String -> Bool
+startsWithSixZeros ('0':'0':'0':'0':'0':'0':_) = True
+startsWithSixZeros _ = False
+
 main = do
     salt <- getLine
-    return $ findIndex startsWithFiveZeros (hashes $ keys salt)
+    print $ "Five zeroes: " ++ show (findIndex startsWithFiveZeros (hashes $ keys salt))
+    print $ "Six zeroes:  " ++ show (findIndex startsWithSixZeros  (hashes $ keys salt))
 
