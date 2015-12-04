a4696ec52c2af64af2135bc01a6974a14eae05d5
carmethene <carmethene@gmail.com>
2015-12-04 12:26:46 -0800

    Use Data.String.Utils' startswith
    Way cleaner than custom solution

--- 4/main.hs
+++ 4/main.hs
@@ -1,4 +1,5 @@
 import Data.List
+import Data.String.Utils
 import qualified Data.Hash.MD5 as MD5
 
 keys :: String -> [String]
@@ -7,16 +8,8 @@ keys salt = [salt ++ show num | num <- [0,1..]]
 hashes :: [String] -> [String]
 hashes = map (MD5.md5s . MD5.Str)
 
-startsWithFiveZeroes :: String -> Bool
-startsWithFiveZeroes ('0':'0':'0':'0':'0':_) = True
-startsWithFiveZeroes _ = False
-
-startsWithSixZeroes :: String -> Bool
-startsWithSixZeroes ('0':'0':'0':'0':'0':'0':_) = True
-startsWithSixZeroes _ = False
-
 main = do
     salt <- getLine
-    print $ "Five zeroes: " ++ show (findIndex startsWithFiveZeroes (hashes $ keys salt))
-    print $ "Six zeroes:  " ++ show (findIndex startsWithSixZeroes  (hashes $ keys salt))
+    print $ "Five zeroes: " ++ show (findIndex (startswith "00000") (hashes $ keys salt))
+    print $ "Six zeroes:  " ++ show (findIndex (startswith "000000") (hashes $ keys salt))
 
