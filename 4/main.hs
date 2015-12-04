ea594cd5c91fccc495e76ae0f93f78803eb73b2a
carmethene <carmethene@gmail.com>
2015-12-04 12:15:27 -0800

    Fix typo

--- 4/main.hs
+++ 4/main.hs
@@ -7,16 +7,16 @@ keys salt = [salt ++ show num | num <- [0,1..]]
 hashes :: [String] -> [String]
 hashes = map (MD5.md5s . MD5.Str)
 
-startsWithFiveZeros :: String -> Bool
-startsWithFiveZeros ('0':'0':'0':'0':'0':_) = True
-startsWithFiveZeros _ = False
+startsWithFiveZeroes :: String -> Bool
+startsWithFiveZeroes ('0':'0':'0':'0':'0':_) = True
+startsWithFiveZeroes _ = False
 
-startsWithSixZeros :: String -> Bool
-startsWithSixZeros ('0':'0':'0':'0':'0':'0':_) = True
-startsWithSixZeros _ = False
+startsWithSixZeroes :: String -> Bool
+startsWithSixZeroes ('0':'0':'0':'0':'0':'0':_) = True
+startsWithSixZeroes _ = False
 
 main = do
     salt <- getLine
-    print $ "Five zeroes: " ++ show (findIndex startsWithFiveZeros (hashes $ keys salt))
-    print $ "Six zeroes:  " ++ show (findIndex startsWithSixZeros  (hashes $ keys salt))
+    print $ "Five zeroes: " ++ show (findIndex startsWithFiveZeroes (hashes $ keys salt))
+    print $ "Six zeroes:  " ++ show (findIndex startsWithSixZeroes  (hashes $ keys salt))
 
