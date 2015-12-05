caa446340e405d668260aa2a511abadf6ccd04a8
carmethene <carmethene@gmail.com>
2015-12-05 12:12:57 -0800

    Solution to 5.2

--- 5/main.hs
+++ 5/main.hs
@@ -11,14 +11,28 @@ hasInfix :: Eq a => [[a]] -> [a] -> Bool
 hasInfix (x:xs) ys = isInfixOf x ys || hasInfix xs ys
 hasInfix _ _ = False
 
-isNice :: String -> Bool
-isNice str =
+isNice1 :: String -> Bool
+isNice1 str =
     countElements "aeiou" str >= 3 &&
     hasDoubleElement str &&
     not (hasInfix ["ab", "cd", "pq", "xy"] str)
 
+hasRepeatingPair :: Eq a => [a] -> Bool
+hasRepeatingPair (x:y:xys) = isInfixOf [x, y] xys || hasRepeatingPair (y:xys)
+hasRepeatingPair _ = False
+
+hasSpacedRepeat :: Eq a => [a] -> Bool
+hasSpacedRepeat (x:y:z:xyzs) = x == z || hasSpacedRepeat (y:z:xyzs)
+hasSpacedRepeat _ = False
+
+isNice2 :: String -> Bool
+isNice2 str =
+    hasRepeatingPair str &&
+    hasSpacedRepeat str
+
 main = do
     input <- getContents
     let strings = lines input
-    print $ "Number of nice strings: " ++ show (length $ filter isNice strings)
+    print $ "Number of nice strings (1): " ++ show (length $ filter isNice1 strings)
+    print $ "Number of nice strings (2): " ++ show (length $ filter isNice2 strings)
 
