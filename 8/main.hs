7df3159befa9dd2acea9af02994d1cf62432b27b
carmethene <carmethene@gmail.com>
2015-12-07 21:32:55 -0800

    Solution to 8.2

--- 8/main.hs
+++ 8/main.hs
@@ -10,10 +10,22 @@ numMemoryChars str = countWithEscapes (tail $ init str) where
     countWithEscapes ('\\':'x':_:_:xs) = 1 + countWithEscapes xs
     countWithEscapes (x:xs) = 1 + countWithEscapes xs
 
+encodeString :: String -> String
+encodeString str = "\"" ++ encodeChars str ++ "\"" where
+    encodeChars :: String -> String
+    encodeChars [] = []
+    encodeChars ('\\':xs) = "\\\\" ++ encodeChars xs
+    encodeChars ('\"':xs) = "\\\"" ++ encodeChars xs
+    encodeChars (x:xs) = x : encodeChars xs
+
 main = do
-    input <- readFile "input.txt"
+    input <- getContents
     let sentences = lines input
+    -- Part 1
     let c = sum $ map numCodeChars sentences
     let m = sum $ map numMemoryChars sentences
     print $ "Answer 1: " ++ show (c - m)
+    -- Part 2
+    let e = sum $ map (numCodeChars . encodeString) sentences
+    print $ "Answer 2: " ++ show (e - c)
 
