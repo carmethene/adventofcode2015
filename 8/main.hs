ff51355499acaefa324f3c0a96ad702cf2fcc37f
carmethene <carmethene@gmail.com>
2015-12-07 21:35:45 -0800

    Minor clean-up

--- 8/main.hs
+++ 8/main.hs
@@ -2,30 +2,30 @@ numCodeChars :: String -> Int
 numCodeChars = length
 
 numMemoryChars :: String -> Int
-numMemoryChars str = countWithEscapes (tail $ init str) where
+numMemoryChars str = countWithEscapes (tail $ init str) where -- Strip wrapping quotes
     countWithEscapes :: String -> Int
     countWithEscapes [] = 0
-    countWithEscapes ('\\':'\\':xs) = 1 + countWithEscapes xs
-    countWithEscapes ('\\':'"':xs) = 1 + countWithEscapes xs
+    countWithEscapes ('\\':'\\':xs)    = 1 + countWithEscapes xs
+    countWithEscapes ('\\':'"':xs)     = 1 + countWithEscapes xs
     countWithEscapes ('\\':'x':_:_:xs) = 1 + countWithEscapes xs
-    countWithEscapes (x:xs) = 1 + countWithEscapes xs
+    countWithEscapes (x:xs)            = 1 + countWithEscapes xs
 
 encodeString :: String -> String
-encodeString str = "\"" ++ encodeChars str ++ "\"" where
+encodeString str = "\"" ++ encodeChars str ++ "\"" where -- Re-add wrapping quotes
     encodeChars :: String -> String
-    encodeChars [] = []
+    encodeChars []        = []
     encodeChars ('\\':xs) = "\\\\" ++ encodeChars xs
     encodeChars ('\"':xs) = "\\\"" ++ encodeChars xs
-    encodeChars (x:xs) = x : encodeChars xs
+    encodeChars (x:xs)    = x : encodeChars xs
 
 main = do
     input <- getContents
     let sentences = lines input
     -- Part 1
-    let c = sum $ map numCodeChars sentences
-    let m = sum $ map numMemoryChars sentences
-    print $ "Answer 1: " ++ show (c - m)
+    let code    = sum $ map numCodeChars sentences
+    let memory  = sum $ map numMemoryChars sentences
+    print $ "Answer 1: " ++ show (code - memory)
     -- Part 2
-    let e = sum $ map (numCodeChars . encodeString) sentences
-    print $ "Answer 2: " ++ show (e - c)
+    let encoded = sum $ map (numCodeChars . encodeString) sentences
+    print $ "Answer 2: " ++ show (encoded - code)
 
