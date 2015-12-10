dcd3e76ee909dbc5d841b2b539c3033aa5590cac
carmethene <carmethene@gmail.com>
2015-12-09 21:24:03 -0800

    Solution to 10.2

--- 10/main.hs
+++ 10/main.hs
@@ -9,7 +9,10 @@ lookAndSay :: String -> String
 lookAndSay val = flatten $ map (\x -> show (length x) ++ [head x]) (group val)
 
 main = do
-   input <- getLine
-   let sq = input : [lookAndSay x | x <- sq]
-   print $ "Length of #40: " ++ show (length $ sq !! 40)
+    input <- getLine
+    let sq = input : [lookAndSay x | x <- sq]
+    -- Part 1
+    print $ "Length of #40: " ++ show (length $ sq !! 40)
+    -- Part 2
+    print $ "Length of #50: " ++ show (length $ sq !! 50)
 
