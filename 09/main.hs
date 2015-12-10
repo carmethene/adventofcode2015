37987ef1ba5ae467d08dfa072148592c600118b0
carmethene <carmethene@gmail.com>
2015-12-09 21:45:27 -0800

    Rewrite with concatMap instead of flatten

--- 09/main.hs
+++ 09/main.hs
@@ -8,11 +8,6 @@ type Road = (Place, Distance)
 type Graph = Map.Map Place [Road]
 type Route = [Place]
 
-flatten :: [[a]] -> [a]
-flatten [] = []
-flatten [a] = a
-flatten (x:xs) = x ++ flatten xs
-
 loadRoad :: String -> Graph -> Graph
 loadRoad string graph = addRoad x y d (addRoad y x d graph) where
     [x, "to", y, "=", ds] = words string
@@ -27,7 +22,7 @@ loadRoad string graph = addRoad x y d (addRoad y x d graph) where
 routes :: Graph -> [Route]
 routes graph = filter (\x -> length x == Map.size graph) allRoutes where
     allRoutes :: [Route]
-    allRoutes = flatten [walkPlace graph place [place] | place <- Map.keys graph] where
+    allRoutes = concatMap (\x -> walkPlace graph x [x]) (Map.keys graph) where
         walkPlace :: Graph -> Place -> Route -> [Route]
         walkPlace graph place route = routes where
             remainingGraph = Map.delete place graph
@@ -35,7 +30,7 @@ routes graph = filter (\x -> length x == Map.size graph) allRoutes where
             nextPlaces = filter (\p -> isJust $ Map.lookup p remainingGraph) [p | (p, _) <- roads]
             routes = case nextPlaces of
                        [] -> [route]
-                       _  -> flatten [walkPlace remainingGraph p (route ++ [p]) | p <- nextPlaces]
+                       _  -> concatMap (\x -> walkPlace remainingGraph x (route ++ [x])) nextPlaces
 
 routeLength :: Graph -> Route -> Distance
 routeLength graph route = sum [getDistance x y | (x, y) <- zip route (tail route)] where
