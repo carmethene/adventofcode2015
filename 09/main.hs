d0d877ce32c8a604bff41c9ee3aedd14b9c83c4a
carmethene <carmethene@gmail.com>
2015-12-10 00:06:04 -0800

    Formatting

--- 09/main.hs
+++ 09/main.hs
@@ -14,29 +14,29 @@ loadRoad string graph = addRoad x y d (addRoad y x d graph) where
     d = read ds
     addRoad :: Place -> Place -> Distance -> Graph -> Graph
     addRoad place destination distance graph = Map.insert place roads graph where
-        road = (destination, distance)
+        road  = (destination, distance)
         roads = case Map.lookup place graph of
                   Just rs -> road : rs
                   Nothing -> [road]
 
 routes :: Graph -> [Route]
-routes graph = filter (\x -> length x == Map.size graph) allRoutes where
-    allRoutes :: [Route]
-    allRoutes = concatMap (\x -> walkPlace graph x [x]) (Map.keys graph) where
-        walkPlace :: Graph -> Place -> Route -> [Route]
-        walkPlace graph place route = routes where
+routes graph = filter (\x -> length x == Map.size graph) everyRoute where
+    everyRoute :: [Route]
+    everyRoute = concatMap (\x -> traverseRoutes graph x [x]) (Map.keys graph) where
+        traverseRoutes :: Graph -> Place -> Route -> [Route]
+        traverseRoutes graph place route = routes where
             remainingGraph = Map.delete place graph
-            roads = fromMaybe [] (Map.lookup place graph)
-            nextPlaces = filter (\p -> isJust $ Map.lookup p remainingGraph) [p | (p, _) <- roads]
-            routes = case nextPlaces of
-                       [] -> [route]
-                       _  -> concatMap (\x -> walkPlace remainingGraph x (route ++ [x])) nextPlaces
+            roads          = fromMaybe [] (Map.lookup place graph)
+            nextPlaces     = filter (\p -> isJust $ Map.lookup p remainingGraph) [p | (p, _) <- roads]
+            routes         = case nextPlaces of
+                               [] -> [route]
+                               _  -> concatMap (\x -> traverseRoutes remainingGraph x (route ++ [x])) nextPlaces
 
 routeLength :: Graph -> Route -> Distance
 routeLength graph route = sum [getDistance x y | (x, y) <- zip route (tail route)] where
     getDistance :: Place -> Place -> Distance
     getDistance x y = distance where
-        roads = fromJust $ Map.lookup x graph
+        roads         = fromJust $ Map.lookup x graph
         (_, distance) = fromJust $ find (\(r,_) -> r == y) roads
 
 main = do
