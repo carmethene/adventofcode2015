79688fc396b62de1057f480254ebc72bdbd30255
carmethene <carmethene@gmail.com>
2015-12-10 22:53:16 -0800

    Formatting

--- 11/main.hs
+++ 11/main.hs
@@ -3,18 +3,18 @@ import Data.List
 incrementString :: String -> String
 incrementString = reverse . incrementWithCarry True . reverse where
     incrementWithCarry :: Bool -> String -> String
-    incrementWithCarry _ []     = []
+    incrementWithCarry _ []         = []
     incrementWithCarry carry (x:xs) = if carry then incrementChar x : rest else x : rest where
         incrementChar :: Char -> Char
-        incrementChar c = if c == 'z' then 'a' else succ c
+        incrementChar chr = if chr == 'z' then 'a' else succ chr
         newCarry = carry && x == 'z'
-        rest = incrementWithCarry newCarry xs
+        rest     = incrementWithCarry newCarry xs
 
 isCompliant :: String -> Bool
 isCompliant str = increasingStraightThree str && noBadChars str && containsTwoPairs str where
     increasingStraightThree :: String -> Bool
     increasingStraightThree (x:y:z:xyzs) = (succ x == y && succ y == z) || increasingStraightThree (y:z:xyzs)
-    increasingStraightThree _ = False
+    increasingStraightThree _            = False
     noBadChars :: String -> Bool
     noBadChars str = null ("iol" `intersect` str)
     containsTwoPairs :: String -> Bool 
