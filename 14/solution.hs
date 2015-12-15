3ef9c67e294e1ad141df63407d8e928f0bba6fc7
carmethene <carmethene@gmail.com>
2015-12-15 15:25:32 -0800

    Solution to 14.2

--- 14/solution.hs
+++ 14/solution.hs
@@ -4,6 +4,7 @@
 import qualified Data.Map as Map
 import qualified Data.ByteString as BS
 import qualified Data.Attoparsec.ByteString.Char8 as A
+import Data.List
 
 -- Types
 type Name     = BS.ByteString
@@ -47,13 +48,25 @@ progressAtTime time moves = distance  where
         nextState :: State -> Move -> State
         nextState (_, _, t0, d0, _) (s, t) = (t0, d0, t0 + t, d0 + s*t, s)
 
+type Point = Integer
+
+tallyPoints :: [[Distance]] -> [Point]
+tallyPoints progress = totalPoints where
+    totalPoints      = map sum $ transpose individualPoints
+    individualPoints = map awardPoints $ transpose progress
+    awardPoints :: [Distance] -> [Point]
+    awardPoints ds = map (awardPoint $ maximum ds) ds
+    awardPoint :: Distance -> Distance -> Point
+    awardPoint max d = if max == d then 1 else 0
+
 main = do
-    input <- BS.readFile "input.txt"
+    input <- BS.getContents
     let (Right reindeer) = A.parseOnly parseReindeerMap input
     -- Part 1
     let progressMap = Map.map (progressAtTime 2503) reindeer
     print $ "Max distance: " ++ show (maximum $ Map.elems progressMap)
     -- Part 2
-    let distanceMap = Map.map (\r -> map (`progressAtTime` r) [0..]) reindeer
-    print $ "Max points:   " ++ show (0)
+    let distanceMap = Map.map (\r -> map (`progressAtTime` r) [1..2503]) reindeer
+    let points      = tallyPoints (Map.elems distanceMap)
+    print $ "Max points:   " ++ show (maximum points)
 
