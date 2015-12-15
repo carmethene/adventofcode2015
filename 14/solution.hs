956779b83375a68b04cf3b99578687bcee750867
carmethene <carmethene@gmail.com>
2015-12-15 15:00:16 -0800

    Bug fixing

--- 14/solution.hs
+++ 14/solution.hs
@@ -35,22 +35,25 @@ parseReindeerMap = do
 
 -- Solver
 
--- A move: (StartDistance, EndDistance, TimeMoveStarted, SpeedOfMove)
-type State = (Distance, Distance, Time, Speed)
+-- A move: (StartTime, StartDistance, EndTime, EndDistance, SpeedOfMove)
+type State = (Time, Distance, Time, Distance, Speed)
 
 progressAtTime :: Time -> [Move] -> Distance
 progressAtTime time moves = distance  where
     distance       = d0 + s * (time - t0)
-    (d0, _, t0, s) = head $ dropWhile (\(_, _, t, _) -> t < time) (stateList moves)
-
-stateList :: [Move] -> [State]
-stateList = scanl nextState (0, 0, 0, 0) . cycle where
-    nextState :: State -> Move -> State
-    nextState (_, d0, t0, _) (s, t) = (d0, d0 + s*t, t0 + t, s)
+    (t0, d0, _, _, s) = head $ dropWhile (\(_, _, t, _, _) -> t < time) (stateList moves)
+    stateList :: [Move] -> [State]
+    stateList = tail . scanl nextState (0, 0, 0, 0, 0) . cycle where
+        nextState :: State -> Move -> State
+        nextState (_, _, t0, d0, _) (s, t) = (t0, d0, t0 + t, d0 + s*t, s)
 
 main = do
     input <- BS.readFile "input.txt"
     let (Right reindeer) = A.parseOnly parseReindeerMap input
+    -- Part 1
     let progressMap = Map.map (progressAtTime 2503) reindeer
     print $ "Max distance: " ++ show (maximum $ Map.elems progressMap)
+    -- Part 2
+    let distanceMap = Map.map (\r -> map (`progressAtTime` r) [0..]) reindeer
+    print $ "Max points:   " ++ show (0)
 
