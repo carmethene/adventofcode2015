0fc01c2e62b209091d12097a754541c1f0edfeb1
carmethene <carmethene@gmail.com>
2015-12-17 15:57:49 -0800

    Solution to 16.1

--- 16/solution.hs
+++ 16/solution.hs
@@ -32,8 +32,20 @@ parseAunts :: A.Parser [Aunt]
 parseAunts = A.many' parseAunt
  
 -- Solver
+auntMatchesSample :: [Sample] -> Aunt -> Bool
+auntMatchesSample [] _ = True
+auntMatchesSample (x:xs) aunt@(_, as) = match && auntMatchesSample xs aunt where
+    (tc, tv) = x
+    match = case lookup tc as of
+      Nothing -> True
+      Just av -> av == tv
+
+desiredAunt = "children: 3, cats: 7, samoyeds: 2, pomeranians: 3, akitas: 0, vizslas: 0, goldfish: 5, trees: 3, cars: 2, perfumes: 1"
+
 main = do
-    input <- BS.readFile "test.txt"
-    let (Right aunts) = A.parseOnly parseAunts input
-    print aunts
+    input <- BS.readFile "input.txt"
+    let (Right aunts)   = A.parseOnly parseAunts input
+    let (Right desired) = A.parseOnly (parseSample `A.sepBy` A.char ',') desiredAunt
+    let matchingAunts = filter (auntMatchesSample desired) aunts
+    print $ "Matching aunt: " ++ show (fst (head matchingAunts))
 
