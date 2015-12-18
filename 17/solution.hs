8eb059d7b3f352f48fe3579338b0d0ae2b5390a7
carmethene <carmethene@gmail.com>
2015-12-17 22:47:38 -0800

    Solution to 17.2

--- 17/solution.hs
+++ 17/solution.hs
@@ -1,16 +1,22 @@
 import Data.List
 
-combinations :: Int -> [Int] -> [[Int]]
-combinations target [] = []
-combinations target (x:xs)
+combinationsOfSize :: Int -> [Int] -> [[Int]]
+combinationsOfSize target [] = []
+combinationsOfSize target (x:xs)
     | x >  target = []
     | x == target = [x] : restWithoutSelf
     | x <  target = restWithSelf ++ restWithoutSelf where
-        restWithSelf    = map ([x] ++) $ combinations (target - x) xs
-        restWithoutSelf = combinations target xs
+        restWithSelf    = map ([x] ++) $ combinationsOfSize (target - x) xs
+        restWithoutSelf = combinationsOfSize target xs
 
 main = do
     input <- readFile "input.txt"
-    let containers = sort $ map (read :: String -> Int) $ lines input
-    print $ "Number of combinations: " ++ show (length (combinations 150 containers))
+    let containers   = sort $ map (read :: String -> Int) $ lines input
+    let combinations = combinationsOfSize 150 containers
+    -- Part 1
+    print $ "Number of combinations: " ++ show (length combinations)
+    -- Part 2
+    let minLength = minimum (map length combinations)
+    let minCombinations = filter ((== minLength) . length) combinations
+    print $ "Minimum combinations:   " ++ show (length minCombinations)
 
