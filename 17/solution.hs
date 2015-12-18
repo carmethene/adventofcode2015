038de42451c478872291ec3261adddbbeb732837
carmethene <carmethene@gmail.com>
2015-12-17 22:38:05 -0800

    Solution to 17.1

--- 17/solution.hs
+++ 17/solution.hs
@@ -1,5 +1,16 @@
+import Data.List
+
+combinations :: Int -> [Int] -> [[Int]]
+combinations target [] = []
+combinations target (x:xs)
+    | x >  target = []
+    | x == target = [x] : restWithoutSelf
+    | x <  target = restWithSelf ++ restWithoutSelf where
+        restWithSelf    = map ([x] ++) $ combinations (target - x) xs
+        restWithoutSelf = combinations target xs
+
 main = do
     input <- readFile "input.txt"
-    let containers = map (read :: String -> Int) $ lines input
-    print containers
+    let containers = sort $ map (read :: String -> Int) $ lines input
+    print $ "Number of combinations: " ++ show (length (combinations 150 containers))
 
