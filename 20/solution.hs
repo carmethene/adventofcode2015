f7c79c48409c453eedcdfa230b14a3033e18f7bf
Neil Wakefield <neil@munkyfun.com>
2015-12-24 10:21:56 +0000

    Finish 20

--- 20/solution.hs
+++ 20/solution.hs
@@ -7,10 +7,15 @@ isqrt = floor . sqrt . fromIntegral
 factors :: Int -> [Int]
 factors n = nub . concat $ [[x, q] | x <- [1..isqrt(n)], let (q, r) = divMod n x, r == 0]
 
-presents :: Int -> Int
-presents = (10 *) . sum . factors
+presents1 :: Int -> Int
+presents1 = (10 *) . sum . factors
+
+presents2 :: Int -> Int
+presents2 n = (11 *) . sum . filter (> (n - 1) `div` 50) . factors $ n
 
 main = do
     target <- read <$> readFile "input.txt"
     -- Part 1
-    print $ findIndex (>= target) (map presents [0..])
+    print $ "House 1: " ++ show (findIndex (>= target) (map presents1 [0..]))
+    -- Part 2
+    print $ "House 2: " ++ show (findIndex (>= target) (map presents2 [0..]))
